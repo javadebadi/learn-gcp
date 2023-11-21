d1b9736c9d34d7736395e6f21ea349636ebff889
javadebadi <javad.ebadi.1990@gmail.com>
2023-11-20 22:28:33 -0700

    Added Lab4

---
+++ DevSecOps-LearningPath/Getting-Started-with-Terraform-for-Google-Cloud/Lab4-Creating-a-Remote-Backend/main_2.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  project     = "qwiklabs-gcp-03-38b5eabdbae4"
+  region      = "us-central-1"
+}
+resource "google_storage_bucket" "test-bucket-for-state" {
+  name        = "qwiklabs-gcp-03-38b5eabdbae4"
+  location    = "US"
+  uniform_bucket_level_access = true
+}
+terraform {
+  backend "gcs" {
+    bucket  = "qwiklabs-gcp-03-38b5eabdbae4"
+    prefix  = "terraform/state"
+  }
+}
