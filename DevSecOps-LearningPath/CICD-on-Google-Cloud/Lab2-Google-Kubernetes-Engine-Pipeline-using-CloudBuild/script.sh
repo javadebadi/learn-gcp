ffb8bc6fa91729e81f0fc65e352c234eb2ae070c
javadebadi <javad.ebadi.1990@gmail.com>
2023-11-30 23:56:08 -0700

    Added

--- DevSecOps-LearningPath/CICD-on-Google-Cloud/Lab2-Google-Kubernetes-Engine-Pipeline-using-CloudBuild/script.sh
+++ DevSecOps-LearningPath/CICD-on-Google-Cloud/Lab2-Google-Kubernetes-Engine-Pipeline-using-CloudBuild/script.sh
@@ -1,2 +1,8 @@
 COMMIT_ID="$(git rev-parse --short=7 HEAD)"
 terraform apply -var container_tag_name=$COMMIT_ID
+
+cd ~ && gcloud source repos clone hello-cloudbuild-env && cd ~/hello-cloudbuild-env && git checkout -b production
+
+cd ~/hello-cloudbuild-env && cp ~/hello-cloudbuild-app/cloudbuild-delivery.yaml ~/hello-cloudbuild-env/cloudbuild.yaml && git add . && git commit -m "Create cloudbuild.yaml for deployment"
+
+git checkout -b candidate && git push origin production && git push origin candidate
