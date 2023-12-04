2fa5a75f73b708fb1f7b00229b6f2cbcbc9083fc
javadebadi <javad.ebadi.1990@gmail.com>
2023-12-04 00:47:16 -0700

    Added lab2 of archtecturing with Google Kubernetes Engine: Workloads

---
+++ DevSecOps-LearningPath/Architecting with Google Kubernetes Engine: Workloads/Lab2-Upgrading Google Kubernetes Engine Clusters/main.sh
@@ -0,0 +1,7 @@
+# create cluster with lowest K8 version
+
+# Upgrade cluster version to highest K8 version
+
+# Node Pools with auto-upgrade with automatically upgrade, however
+# to immediately upgrade the node pools you can click on the 
+# "Upgrade Oldest Node Pool" button
