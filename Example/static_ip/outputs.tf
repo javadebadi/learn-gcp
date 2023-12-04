7aa2d83c9be752b4bf66ef381101a29a55919f20
javadebadi <javad.ebadi.1990@gmail.com>
2023-12-03 20:20:40 -0700

    Added staic ip

--- Example/static_ip/outputs.tf
+++ Example/static_ip/outputs.tf
@@ -1,8 +1,8 @@
 
 output "ip_address" {
-    value = google_compute_address.ip_address.address
+  value = google_compute_address.ip_address.address
 }
 
 output "self_link" {
-    value = google_compute_address.ip_address.self_link
+  value = google_compute_address.ip_address.self_link
 }
